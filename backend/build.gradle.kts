5cf77948b212da186ff801de8930659826d00f84
Erik <elerickmontenegro@gmail.com>
2025-06-08 21:49:12 +0200

    use postgres

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
+	runtimeOnly("org.postgresql:postgresql")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("com.h2database:h2")
